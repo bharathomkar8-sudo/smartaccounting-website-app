71b20e7fff42aac5a4c664a421af705f0a0e408f
bharathomkar8-sudo <bharathomkar8@gmail.com>
2026-03-31 23:59:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,10 @@ document.querySelectorAll(".title").forEach(item => {
   item.addEventListener("click", () => {
     let parent = item.parentElement;
 
-    // Close others
     document.querySelectorAll(".dropdown").forEach(d => {
       if (d !== parent) d.classList.remove("open");
     });
 
-    // Remove active from all titles
-    document.querySelectorAll(".title").forEach(t => {
-      t.classList.remove("active");
-    });
-
-    // Open clicked
     parent.classList.toggle("open");
-
-    // Add active style
-    item.classList.add("active");
   });
 });
